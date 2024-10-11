1f9a377ca463ccc834ec46abd522b73c3a60986d
Naeem <naeemkarimzadeh@gmail.com>
2024-10-11 11:21:07 +0330

    project is done

--- project.py
+++ project.py
@@ -1,12 +1,18 @@
 import requests
 import json
 import pyttsx3
-Request = requests.get('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
-JsRequest = json.loads(Request.text)
-bit_coin_UpTime=JsRequest['time']['updated']
-bit_coin_price = JsRequest['bpi']['USD']['rate']
-print ('The price of Bitcoin at time {} is equal to {}'.format(bit_coin_UpTime , bit_coin_price))
-ansForSound = 'The price of Bitcoin at time {} is equal to {}'.format(bit_coin_UpTime , bit_coin_price)
-ttsENGIN= pyttsx3.init()
-ttsENGIN.say(ansForSound)
-ttsENGIN.runAndWait()
+import time
+def create_message():
+    Request = requests.get('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
+    JsRequest = json.loads(Request.text)
+    bit_coin_UpTime=JsRequest['time']['updated']
+    bit_coin_price = JsRequest['bpi']['USD']['rate']
+    ansForSound = 'The price of Bitcoin at time {} is equal to {}'.format(bit_coin_UpTime , bit_coin_price)
+    return ansForSound
+def sey_text():
+    ttsENGIN= pyttsx3.init()
+    ttsENGIN.say(create_message())
+    ttsENGIN.runAndWait()
+while True:
+    sey_text()
+    time.sleep(100)
